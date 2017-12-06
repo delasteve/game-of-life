3518c0d8b4cd835a6d97ac9263702b060a336ec3
Stephen Cavaliere <stephencavaliere@gmail.com>
2017-12-06 11:27:47 -0500

    refactor: move bounds checker to private function and more functional loop

--- src/GameOfLife/Game.cs
+++ src/GameOfLife/Game.cs
@@ -42,43 +42,30 @@ namespace GameOfLife
             {
                 for (var x = 0; x < _grid[y].Count; x++)
                 {
-                    var neighbors = new List<Cell>();
-
-                    try {
-                        neighbors.Add(_grid[y-1][x-1]);
-                    } catch (Exception) { }
-
-                    try {
-                        neighbors.Add(_grid[y-1][x]);
-                    } catch (Exception) { }
-
-                    try {
-                        neighbors.Add(_grid[y-1][x+1]);
-                    } catch (Exception) { }
-
-                    try {
-                        neighbors.Add(_grid[y][x-1]);
-                    } catch (Exception) { }
-
-                    try {
-                        neighbors.Add(_grid[y][x+1]);
-                    } catch (Exception) { }
-
-                    try {
-                        neighbors.Add(_grid[y+1][x-1]);
-                    } catch (Exception) { }
-
-                    try {
-                        neighbors.Add(_grid[y+1][x]);
-                    } catch (Exception) { }
-
-                    try {
-                        neighbors.Add(_grid[y+1][x+1]);
-                    } catch (Exception) { }
-
-                    _grid[y][x] = CellRules.GetNextCellGeneration(_grid[x][y], neighbors);
+                    _grid[y][x] = CellRules.GetNextCellGeneration(_grid[x][y], GetNeighbors(x, y));
                 }
             }
+        }
+
+        private IEnumerable<Cell> GetNeighbors(int x, int y)
+        {
+            var neighbors = new List<Cell>();
+
+            for (int row = -1; row < 2; row++)
+            {
+                for (int col = -1; col < 2; col++)
+                {
+                    if (row == 0 && col == 0) { continue; }
+
+                    try
+                    {
+                        neighbors.Add(_grid[y + row][x + col]);
+                    }
+                    catch (Exception) { }
+                }
+            }
+
+            return neighbors;
         }
 
         private List<List<Cell>> CreateShallowCopyOfGrid()
