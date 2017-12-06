d702002bff7a75eac713b2cb3013e1c5cd82f9b9
Stephen Cavaliere <stephencavaliere@gmail.com>
2017-12-06 12:55:10 -0500

    test: remove building block tests for ToggleState and SpawnNextGeneration

--- tests/GameOfLife.Tests/GameTests.cs
+++ tests/GameOfLife.Tests/GameTests.cs
@@ -22,16 +22,6 @@ namespace GameOfLife.Tests
         }
 
         [Fact]
-        public void ToggleState_GivenAZeroBasedXandYCoordinateAndADeadCell_UpdatesCellToBeAlive()
-        {
-            var game = new Game(2, 2);
-
-            game.ToggleState(0, 0);
-
-            game.Grid.ElementAt(0).ElementAt(0).Should().Be(Cell.ALIVE);
-        }
-
-        [Fact]
         public void ToggleState_GivenAZeroBasedXandYCoordinateAndAnAliveCell_UpdatesCellToBeDead()
         {
             var game = new Game(2, 2);
@@ -43,51 +33,6 @@ namespace GameOfLife.Tests
             game.ToggleState(0, 0);
 
             game.Grid.ElementAt(0).ElementAt(0).Should().Be(Cell.DEAD);
-        }
-
-        [Fact]
-        public void SpawnNextGeneration_GivenAValidGameGrid_ReplacesGridWithCopy()
-        {
-            var game = new Game(2, 2);
-            var oldGrid = game.Grid;
-
-            game.SpawnNextGeneration();
-
-            oldGrid.Should().NotBeSameAs(game.Grid);
-        }
-
-        [Fact]
-        public void SpawnNextGeneration_GivenAValidGameGrid_WillUpdateCellsAccordingToCellRules()
-        {
-            var game = new Game(1, 1);
-            var expectedGrid = new List<List<Cell>>
-            {
-                new List<Cell> { Cell.DEAD }
-            };
-            game.ToggleState(0, 0);
-
-            game.SpawnNextGeneration();
-
-            game.Grid.ShouldBeEquivalentTo(expectedGrid);
-        }
-
-        [Fact]
-        public void SpawnNextGeneration_GivenAValidGameGridWith3AliveNeighbors_WillConsiderNeighborsInNextGeneration()
-        {
-            var game = new Game(2, 2);
-            var expectedGrid = new List<List<Cell>>
-            {
-                new List<Cell> { Cell.ALIVE, Cell.ALIVE },
-                new List<Cell> { Cell.ALIVE, Cell.ALIVE }
-            };
-            game.ToggleState(0, 0);
-            game.ToggleState(1, 0);
-            game.ToggleState(0, 1);
-            game.ToggleState(1, 1);
-
-            game.SpawnNextGeneration();
-
-            game.Grid.ShouldBeEquivalentTo(expectedGrid);
         }
 
         [Fact]
